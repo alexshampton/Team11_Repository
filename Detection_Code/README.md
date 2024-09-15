3e9d6ac8010d31a9c4b5ec0c1de8f85941e879ec
Alexander Shampton <alexandershampton@gmail.com>
2024-09-14 20:36:09 -0400

    Removed initial testing images and added error checking

---
+++ Detection_Code/README.md
@@ -0,0 +1,8 @@
+# Detection Code
+## Dependencies
+* pillow==10.4.0
+
+## Scripts
+* imgToBin.py: Generates binary files for each image in the images folder. 
+    * Input: Requires images in the images directory
+    * Output: Binary files are outputted to the binary directory. The filename is retained during image to binary conversion. 
--- Detection_Code/imgToBin.py
+++ Detection_Code/imgToBin.py
@@ -1,22 +1,42 @@
-# Required Libraries
 from PIL import Image
+import os
 
-def image_to_jabcode(image_path, output_jabcode_path):
-    # Step 1: Load the image using Pillow
-    image = Image.open(image_path)
-    
-    # Step 2: Convert the image into bytes (optional: resize to reduce complexity)
-    image = image.convert('RGB')
-    img_data = image.tobytes()
+# Converts image to binary
+# Uses image path to find image
+def image_binary_data(image_path):
+    try:
+        image = Image.open(image_path)
+        image = image.convert('RGB')
+        img_data = image.tobytes()
+
+    except FileNotFoundError:
+        print(f"Error: File not found - {image_path}")
+
+    except OSError:
+        print(f"Error: Cannot open image (unsupported format or corrupted file) - {image_path}")
+
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
 
     return(img_data)
 
-# Example usage
-file_data = image_to_jabcode("images/test.png", "output_jabcode.png")
+def binary_file_add(imageDirectory, binaryDirectory):
 
-# Open a file in binary write mode
-with open("binary/output.bin", "wb") as binary_file:
-    # Write some binary data to the file
-    binary_file.write(file_data)
+    # Loops through files in the images directory
+    picCheck = 0
+    for file in os.listdir(imageDirectory):
+        picCheck = 1
+        file_data = image_binary_data(imageDirectory + "/" + file)
+        filename = os.path.splitext(file) #splits file by the extension 
 
-print("Binary file written successfully!")
+        # Open and create a file in binary write mode
+        with open(binaryDirectory + '/' + filename[0] + '.bin', 'wb') as binary_file:
+            binary_file.write(file_data)
+
+        print(f"{binaryDirectory + '/' + filename[0]}.bin written successfully!")
+
+    if picCheck == 0:
+        print("No images are in the images directory!")
+
+if __name__ == "__main__":
+    binary_file_add('images', 'binary')
