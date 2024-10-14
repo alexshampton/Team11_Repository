5a41261acb92d479ce5310ec3bce4d1a9eee316f
Alex Shampton <shampton.10@wright.edu>
2024-10-14 16:57:49 -0400

    Updated jabcode shell script that creates a jabcode for the md5sum ouputted by JabcodeReader.sh. Issue #8

--- ServerCode/JabcodeReadWrite/JabcodeWrite/jabcode_md5sum.sh
+++ ServerCode/JabcodeReadWrite/JabcodeWrite/jabcode_md5sum.sh
@@ -10,6 +10,6 @@ make -C $HOME/Team11_Repository/Jabcode/jabcodeRepo/src/jabcodeWriter
 tar -czvf "$COMPRESSED_FILE" md5sum.txt  #Compresses md5sum file
 openssl enc -aes-256-cbc -salt -in "$COMPRESSED_FILE" -out "$ENCRYPTED_FILE" 
 
-$JABCODE_WRITER --input-file "$HOME/Team11_Repository/ServerCode/JabcodeWrite/$ENCRYPTED_FILE" --output "jabcodes/$BASE_NAME.png"
+$JABCODE_WRITER --input-file $ENCRYPTED_FILE --output "jabcodes/$BASE_NAME.png"
 echo "Processed $ENCRYPTED_FILE -> jabcodes/$BASE_NAME.png"
 
