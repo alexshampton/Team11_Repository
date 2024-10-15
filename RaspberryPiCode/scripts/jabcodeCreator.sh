78748f8d457f202148400c661f26d1b6b72214bb
Alex Shampton <shampton.10@wright.edu>
2024-10-15 11:55:11 -0400

    WIP updating .sh paths

--- RaspberryPiCode/scripts/jabcodeCreator.sh
+++ RaspberryPiCode/scripts/jabcodeCreator.sh
@@ -3,18 +3,22 @@
 JABCODE_WRITER="$HOME/Team11_Repository/Jabcode/jabcodeRepo/src/jabcodeWriter/bin/jabcodeWriter"
 
 # Input directory containing binary files
-BIN_SPLIT_DIR="binarySplit"
-COMPRESSED_FILE="Death_Star_Images.tar.gz"
-ENCRYPTED_FILE="Death_Star_Images.tar.gz.enc"
+BIN_SPLIT_DIR="$HOME/Team11_Repository/RaspberryPiCode/assets/splitFiles"
+MD5SUM_DIR="../assets/md5sums"
+COMPRESSED_FILE="../assets/compressedAndZippedFiles/Death_Star_Images.tar.gz"
+ENCRYPTED_FILE="../assets/compressedAndZippedFiles/Death_Star_Images.tar.gz.enc"
+IMAGES_FILE="../assets/death_star_images"
 FILENAME="Death_Star_Images"
 
 # Create output directory if it doesn't exist
-mkdir -p "jabcodes"
-mkdir -p "binarySplit"
+mkdir -p "../assets/dsImagesJabcodes"
+mkdir -p "../assets/splitFiles"
+mkdir -p "../assets/compressedAndZippedFilesRP"
+mkdir -p "../assets/md5sums"
 
 make -C $HOME/Team11_Repository/Jabcode/jabcodeRepo/src/jabcodeWriter
-tar -czvf "$COMPRESSED_FILE" "death_star_images" #Compresses images folder
-md5sum "$COMPRESSED_FILE" > md5sum.txt
+tar -czvf "$COMPRESSED_FILE" "$IMAGES_FILE" #Compresses images folder
+md5sum "$COMPRESSED_FILE" > "$MD5SUM_DIR/md5sumRP.txt"
 openssl enc -aes-256-cbc -salt -in "$COMPRESSED_FILE" -out "$ENCRYPTED_FILE" 
 split -b 4k "$ENCRYPTED_FILE" "$BIN_SPLIT_DIR/$FILENAME"_
 
@@ -25,10 +29,10 @@ for split_file in $BIN_SPLIT_DIR/*; do
     base_name=$(basename "$split_file")
     
     # Define the output file name
-    output_file="$base_name.png"
+    output_file="../assets/dsImagesJabcodes/$base_name.png"
     
     # Run the jabcodeWriter command
-    $JABCODE_WRITER --input-file "$split_file" --output "jabcodes/$output_file"
+    $JABCODE_WRITER --input-file "$split_file" --output "$output_file"
     ((COUNT++))
     # Print status
     echo "Processed $split_file -> $output_file Count: $COUNT"
--- RaspberryPiCode/scripts/readAndCompareMD5SUM.sh
+++ RaspberryPiCode/scripts/readAndCompareMD5SUM.sh
@@ -1,16 +1,25 @@
 JABCODE_READER="$HOME/Team11_Repository/Jabcode/jabcodeRepo/src/jabcodeReader/bin/jabcodeReader"
 
-# Input directory containing binary files
-JABCODE_DIR="$HOME/Team11_Repository/ServerCode/JabcodeReadWrite/JabcodeRead/jabcodes" #CHANGE DIR TO JABCODE IMAGES
-MD5SUM_DIR="$HOME/Team11_Repository/RaspberryPiCode/JabcodeRead"
+# Directories to be used by scripts
+JABCODE_DIR="$HOME/Team11_Repository/RaspberryPiCode/assets/md5sumJabcodes" #CHANGE DIR TO JABCODE IMAGES
+MD5SUM_DIR="$HOME/Team11_Repository/RaspberryPiCode/assets/md5sums"
+CAZ_FILE_DIR="$HOME/Team11_Repository/RaspberryPiCode/assets/compressedAndZippedFilesServer"
+
+# Files to be used by script
+MD5SUM_RP_FILE="../assets/md5sums/md5sumRP.txt"
+MD5SUM_SERVER_FILE="../assets/md5sums/md5sumServer.txt"
+ENCRYPTED_FILE="$CAZ_FILE_DIR/md5sumServer.tar.gz.enc"
+COMPRESSED_FILE="$CAZ_FILE_DIR/md5sumServer.tar.gz"
+JABCODE_MD5SUM_FILE="$JABCODE_DIR/md5sumJabcode.png"
 
 # Create output directory if it doesn't exist
 mkdir -p "encryptedFiles"
-mkdir -p "jabcode"
+mkdir -p "../assets/md5sumJabcodes"
+mkdir -p "../assets/compressedAndZippedFilesServer"
 make -C $HOME/Team11_Repository/Jabcode/jabcodeRepo/src/jabcodeReader
 
-$JABCODE_READER "jabcode/md5sumServer.png" --output "$MD5SUM_DIR/md5sumServer.tar.gz.enc"
+$JABCODE_READER "$JABCODE_MD5SUM_FILE" --output "$ENCRYPTED_FILE"
 
-openssl enc -aes-256-cbc -d -in md5sumServer.tar.gz.enc -out md5sumServer.tar.gz
-tar -xzvf md5sumServer.tar.gz > md5sumServer.txt
-diff md5sumServer.txt ../Binary_Conversion/md5sum.txt
+openssl enc -aes-256-cbc -d -in "$ENCRYPTED_FILE" -out "$COMPRESSED_FILE"
+tar -xzvf "$COMPRESSED_FILE" > "$MD5SUM_SERVER_FILE"
+diff "$MD5SUM_SERVER_FILE" "$MD5SUM_RP_DIR"
